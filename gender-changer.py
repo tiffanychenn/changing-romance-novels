58819a8a1be89572a2133c7b62058175b3be1329
Tiffany Chen <tiffanysta.c@gmail.com>
2021-11-23 02:25:15 -0500

    extra changes

--- gender-changer.py
+++ gender-changer.py
@@ -1,16 +1,15 @@
 import string
 
-other_male_characters = set(["Mr. Bennet", "Mr. Bingley", "Mr. Wickham", "Mr. Collins", "Mr. Gardiner"])
-male_to_female = {"he": "she", "him": "her", "his": "hers", "himself": "herself", "He": "She", "Him": "Her", "His": "Her", "Himself": "Herself"}
+other_male_characters = set(["Bennet", "Bingley", "Wickham", "Collins", "Gardiner"])
+male_to_female = {"he": "she", "him": "her", "his": "her", "himself": "herself", "boy": "girl", "man": "woman", "gentleman": "lady"}
 
 with open('original-text/pride-and-prejudice.txt', 'r') as f:
     text = f.read()
     replaced_text = text.replace("Mr. Darcy", "Ms. Darcy")
-    replaced_text = replaced_text.replace("Fitzwilliam", "Fiona")
+    replaced_text = replaced_text.replace("Mr. Fitzwilliam", "Ms. Fiona")
     replaced_text = replaced_text.replace("“", "\"")
     replaced_text = replaced_text.replace("”", "\"")
     new_text = ""
-    last_word = ""
     at_darcy = False
     for paragraph in replaced_text.split("\n"):
         for word in paragraph.split(" "):
@@ -22,11 +21,15 @@ with open('original-text/pride-and-prejudice.txt', 'r') as f:
             elif len(new_word) > 0 and new_word[-1] in string.punctuation:
                 punc = new_word[-1]
                 new_word = new_word[:-1]
-            if at_darcy and new_word in male_to_female:
-                new_word = male_to_female[new_word]
-            if "Darcy" in word:
+            if at_darcy and new_word.lower() in male_to_female:
+                female_word = male_to_female[new_word.lower()]
+                if ord(new_word.strip(string.punctuation)[0]) < 96:
+                    new_word = chr(ord(female_word[0]) - 32) + female_word[1:]
+                else:
+                    new_word = female_word
+            if "Darcy" in new_word:
                 at_darcy = True
-            elif last_word == "Mr." and "Darcy" not in new_word:
+            elif new_word.strip(string.punctuation) in other_male_characters:
                 at_darcy = False
             new_text += new_word + punc + " "
         new_text = new_text[:-1] + "\n"
